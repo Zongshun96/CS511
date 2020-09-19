be3338099761372b78549d313a6d7333872ed4f4
zhan4475 <zhan4475@umn.edu>
2020-09-18 23:22:49 -0400

    clean code

--- hw2/parity.py
+++ hw2/parity.py
@@ -7,7 +7,7 @@ p1,p2,p3,p4 = Bools('p1 p2 p3 p4')
 s = Solver()
 
 
-phi = And (Or(Not(p1), p2, p3, p4), Or(p1, Not(p2), p3, p4), Or (p1, p2, Not (p3), p4), Or(p1, p2, p3, (Not (p4))),
+phi = And (Or(Not(p1), p2, p3, p4), Or(p1, Not(p2), p3, p4), Or (p1, p2, Not(p3), p4), Or(p1, p2, p3, Not(p4)),
             Or(Not(p1), Not(p2), Not(p3), p4), Or(Not(p1), Not(p2), p3, Not (p4)), Or (Not(p1), p2, Not(p3), Not (p4)), Or(p1, Not (p2), Not (p3), Not (p4)))
 psi = Or(And(p1, p2, p3, p4), And(Not(p1), Not(p2), Not(p3), Not(p4)), And(p1, p2, Not(p3), Not(p4)), And(Not(p1), Not(p2), p3, p4),
          And(Not(p1), p2, Not(p3), p4), And(Not(p1), p2, p3, Not(p4)), And(p1, Not(p2), Not(p3), p4), And(p1, Not(p2), p3, Not(p4)))
