8673a31b35b8b685ab253b09211339f56f4cf99d
zhan4475 <zhan4475@umn.edu>
2020-09-18 23:39:34 -0400

    doc code

--- hw2/parity.py
+++ hw2/parity.py
@@ -6,15 +6,17 @@ p1,p2,p3,p4 = Bools('p1 p2 p3 p4')
 
 s = Solver()
 
-
-phi = And (Or(Not(p1), p2, p3, p4), Or(p1, Not(p2), p3, p4), Or (p1, p2, Not(p3), p4), Or(p1, p2, p3, Not(p4)),
-            Or(Not(p1), Not(p2), Not(p3), p4), Or(Not(p1), Not(p2), p3, Not (p4)), Or (Not(p1), p2, Not(p3), Not (p4)), Or(p1, Not (p2), Not (p3), Not (p4)))
+# match F cases in each clause
+phi = And(Or(Not(p1), p2, p3, p4), Or(p1, Not(p2), p3, p4), Or (p1, p2, Not(p3), p4), Or(p1, p2, p3, Not(p4)),
+            Or(Not(p1), Not(p2), Not(p3), p4), Or(Not(p1), Not(p2), p3, Not(p4)), Or(Not(p1), p2, Not(p3), Not(p4)), Or(p1, Not(p2), Not(p3), Not(p4)))
+# match T cases in each clause
 psi = Or(And(p1, p2, p3, p4), And(Not(p1), Not(p2), Not(p3), Not(p4)), And(p1, p2, Not(p3), Not(p4)), And(Not(p1), Not(p2), p3, p4),
          And(Not(p1), p2, Not(p3), p4), And(Not(p1), p2, p3, Not(p4)), And(p1, Not(p2), Not(p3), p4), And(p1, Not(p2), p3, Not(p4)))
+# same as XOR
 theta = p1 == (p2 == (p3 == p4))
 
 # # constraint: check sat for not equal
-s.add(Not (phi == theta))
-s.add(Not (theta == psi))
-s.add(Not (psi == phi))
+s.add(Not(phi == theta))
+s.add(Not(theta == psi))
+s.add(Not(psi == phi))
 print (s.check())
--- hw2/parity.smt2
+++ hw2/parity.smt2
@@ -1,4 +1,4 @@
-;;;;   execute at the linux prompt by issuing the command:
+;;;;   execute at the terminal by issuing the command:
 ;;;;   > z3 parity.smt2
 
 (declare-const p1 Bool)
@@ -6,16 +6,17 @@
 (declare-const p3 Bool)
 (declare-const p4 Bool)
 
+;; match F cases in each clause
 (declare-fun phi (Bool Bool Bool Bool) Bool)
 (assert (= (phi p1 p2 p3 p4)
            (and (or (not p1) p2 p3 p4) (or p1 (not p2) p3 p4) (or p1 p2 (not p3) p4) (or p1 p2 p3 (not p4))
             (or (not p1) (not p2) (not p3) p4) (or (not p1) (not p2) p3 (not p4)) (or (not p1) p2 (not p3) (not p4)) (or p1 (not p2) (not p3) (not p4)))))
-
+;; match T cases in each clause
 (declare-fun psi (Bool Bool Bool Bool) Bool)
 (assert (= (psi p1 p2 p3 p4)
            (or (and p1 p2 p3 p4) (and (not p1) (not p2) (not p3) (not p4)) (and p1 p2 (not p3) (not p4)) (and (not p1) (not p2) p3 p4)
             (and (not p1) p2 (not p3) p4) (and (not p1) p2 p3 (not p4)) (and p1 (not p2) (not p3) p4) (and p1 (not p2) p3 (not p4)))))
-
+;; same as XOR
 (declare-fun theta (Bool Bool Bool Bool) Bool)
 (assert (= (theta p1 p2 p3 p4)
            (= p1 (= p2 (= p3 p4)))))
