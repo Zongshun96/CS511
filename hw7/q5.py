3257e22ddb9065340c6ab96422202fc8bd594304
zhan4475 <zhan4475@umn.edu>
2020-10-30 19:47:32 -0400

    hw7

--- hw7/q5.py
+++ hw7/q5.py
@@ -1,25 +1,46 @@
+# please use the same format as in the sample lists, and make sure the working directory has the readin file
+# to run: python3 q5.py
+
+
+import ast
 from z3 import *
 opt = Optimize() # find min
 
 variable_d = {}
 neg_var_d = {}
 
-# input = [
+
+# input_list = [
 #             [ [1,[[0,1]]], [1,[[0,2]]] ] ,
 #             [ [1,[[0,1],[1,2]]], [2,[[0,2]]], [-3,[[0,1]]], [-1,[]] ] ,
 #             [ [-1,[[0,1],[1,2]]], [-2,[[0,2]]], [3,[[0,1]]], [1,[]] ]
 #         ]
 
-input = [
+# input_list = [
+#             [ [1, [[0,1]]] ] ,
+#             [ [1, [[0,1]]] ] ,
+#             [ [-1,[[0,1]]] ]
+#         ]
+
+input_list = [
             [ [1, [[0,1]]] ] ,
-            [ [1, [[0,1]]] ] ,
-            [ [-1,[[0,1]]] ]
+            [ [1, [[0,1],[0,1]]], [-1,[[0,1]] ]]  ,
+            [ [-1,[[0,1],[0,1]]], [1, [[0,1]] ]]  
         ]
 
-for i in range(len(input)):
+try:
+    filename = input("type the filename or use default input: ")
+    with open("./"+filename, 'r') as f:
+        input_list = ast.literal_eval(f.read())
+except Exception as e:
+    print(e)
+    print("go with default")
+
+print(input_list)
+for i in range(len(input_list)):
     # init vars
     if i == 0:
-        for term in input[i]:
+        for term in input_list[i]:
             for var in term[1]:
                 # prepare the x and its negation vars
                 variable_d[var[1]] = Int("x"+str(var[1]))
@@ -30,7 +51,7 @@ for i in range(len(input)):
     # init minimization
     if i == 0:
         to_minimize = 0
-        for term in input[i]:
+        for term in input_list[i]:
             term_to_minimize = 1
             for var in term[1]:
                 if var[0] == 0:
@@ -42,7 +63,7 @@ for i in range(len(input)):
         opt.minimize(to_minimize)
     else:
         new_constraint = 0
-        for term in input[i]:
+        for term in input_list[i]:
             term_new_constraint = 1
             for var in term[1]:
                 if var[0] == 0:
@@ -55,4 +76,4 @@ for i in range(len(input)):
             
 print(opt.check())
 print(opt.model())
-
+print(opt.check()==sat)
